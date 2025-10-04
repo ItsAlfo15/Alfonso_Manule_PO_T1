68e21e73e2364dd31962226f2d218f2c78683471
ManuelJesus2006 <manulecuentatwitch@gmail.com>
2025-10-04 10:51:57 +0200

    Commit main mejorado

--- bin/view/Main.dart
+++ bin/view/Main.dart
@@ -1,22 +1,69 @@
 import 'dart:io';
 
-import '../controller/app_manager.dart';
+import '../constructor/AppManager.dart';
+import '../models/paciente.dart';
 
 void main() {
   AppManager controlador = AppManager();
   int op = 0;
 
-  // Leer la línea como String? y parsear a int
-  String? input = stdin.readLineSync();
+  do {
+    String? input = stdin.readLineSync();
   if (input == null) {
     print('No se recibió entrada');
   } else {
     int? parsed = int.tryParse(input);
     if (parsed == null) {
-      print('Entrada no válida: $input');
+      print('Valor incorrecto, intentaló de nuevo');
     } else {
       op = parsed;
-      print(op);
+      switch(op){
+        case 1: //Admisión de paciente
+          admisionPaciente(controlador);
+          break;
+        case 2: //Liberar
+          liberarConsulta(controlador);
+          break;
+        case 3: //Ver cola
+          pintaCola(controlador);
+          break;
+        case 4: //Ver estado consultas
+          pintaConsultas(controlador);
+          break;
+        case 5: //Busqueda
+          buscaPacientes(controlador);
+          break;
+      }
+    }
+  }
+  } while (op != 6);
+  
+}
+
+Function admisionPaciente(AppManager controlador){
+  String? dni = stdin.readLineSync();
+  String? nombre = stdin.readLineSync();
+  String? apellidos = stdin.readLineSync();
+  String? sintomas = stdin.readLineSync();
+  int resultado = controlador.admisionPaciente(nombre, apellidos, dni, sintomas);
+  if (resultado == -1) print("No hay ninguna consulta libre, debe esperar en la cola");
+  else print("Se le ha asignado la consulta $resultado, ya puede pasar. Le atenderá ${controlador.nombreMedicoEnConsulta(resultado)}");
+}
+
+Function liberarConsulta(AppManager controlador){
+  print("Introduce la consulta que ha sido liberada: ");
+  String? consultaLibreNoParse = stdin.readLineSync();
+  if (consultaLibreNoParse == null){
+    print("Ocurrió un error vuelva a intenarlo");
+  }else{
+    int? consultaLibreParse = int.tryParse(consultaLibreNoParse);
+    if (consultaLibreParse == null) print("Ocurrió un error vuelva a intenarlo");
+    else{
+      if (!controlador.consultaValidaParaLiberar(consultaLibreParse)) print("El número de consulta no es válido");
+      else{
+        Paciente siguiente = controlador.liberaConsulta(consultaLibreParse);
+        if (siguiente == null) print("");
+      }
     }
   }
 }
