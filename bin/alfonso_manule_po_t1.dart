46424de5e66930b199063f18e715c811a6313c14
ItsAlfo15 <alfo.15.play@gmail.com>
2025-10-04 09:51:13 +0200

    Prueba

--- bin/alfonso_manule_po_t1.dart
+++
@@ -1,5 +0,0 @@
-import 'package:alfonso_manule_po_t1/alfonso_manule_po_t1.dart' as alfonso_manule_po_t1;
-
-void main(List<String> arguments) {
-  print('Hello world: ${alfonso_manule_po_t1.calculate()}!');
-}
