7f364f92534c1c00b1fdf63bb65e55e76252216d
ItsAlfo15 <alfo.15.play@gmail.com>
2025-10-04 09:42:34 +0200

    Primer commit

---
+++ bin/alfonso_manule_po_t1.dart
@@ -0,0 +1,5 @@
+import 'package:alfonso_manule_po_t1/alfonso_manule_po_t1.dart' as alfonso_manule_po_t1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${alfonso_manule_po_t1.calculate()}!');
+}
---
+++ test/alfonso_manule_po_t1_test.dart
@@ -0,0 +1,8 @@
+import 'package:alfonso_manule_po_t1/alfonso_manule_po_t1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
