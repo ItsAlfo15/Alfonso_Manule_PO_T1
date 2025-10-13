19b3be43ebdb597b3b5e378bffd630be55a46a2a
ItsAlfo15 <alfo.15.play@gmail.com>
2025-10-13 10:51:11 +0200

    Prueba2

--- bin/providers/medicos_provider.dart
+++ bin/providers/medicos_provider.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart';
 import '../models/medico_model.dart';
 
 final _urlBase =
-    'https://alfonso-manule-po-t1-default-rtdb.europe-west1.firebasedatabase.app/Medicos';
+    'https://alfonso-manule-po-t1-default-rtdb.europe-west1.firebasedatabase.app/Medicos.json';
 
 Future<List<dynamic>> getMedicos() async {
   List<dynamic> listaMedicos = [];
@@ -17,7 +17,7 @@ Future<List<dynamic>> getMedicos() async {
 
   final Map<String, dynamic> resp = jsonDecode(response.body);
 
-  print(resp);
+  resp.forEach(action)
 
   return listaMedicos;
 }
