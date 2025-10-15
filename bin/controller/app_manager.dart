d60d317550f8e3e93cd90047451ad6ab1bb9464c
ItsAlfo15 <alfo.15.play@gmail.com>
2025-10-15 20:28:22 +0200

    Algunos metodos del controlador

--- bin/controller/app_manager.dart
+++ bin/controller/app_manager.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:developer';
 
 import '../models/paciente_model.dart';
 import '../models/medico_model.dart';
@@ -16,14 +17,66 @@ class AppManager {
 
   // Getters y Setters
 
-  getDatosControlador() async{
+  getDatosControlador() async {
     medicos = await getMedicos();
     pacientes = await getPacientes();
     consultas = await getConsultas();
   }
 
   // Constructor
-  AppManager() {
-  
+  AppManager();
+
+  int numConsultas() {
+    return consultas.length;
+  }
+
+  int numConsultasLibres() {
+    int cont = 0;
+    consultas.forEach((consulta) {
+      if (consulta.idPaciente == null) cont++;
+    });
+    return cont;
+  }
+
+  int numPacientesEnCola() {
+    return -1;
+  }
+
+  int numPacientesCurados() {
+    return -1;
+  }
+
+  Medico? buscaMedicoByID(String idMedicoPasado) {
+    Medico? temp;
+
+    medicos.forEach((medico) {
+      if (medico.idMedico == idMedicoPasado) temp = medico;
+    });
+
+    return temp;
+  }
+
+  Paciente? buscaPacienteByID(String idPacientePasado) {
+    Paciente? temp;
+
+    pacientes.forEach((paciente) {
+      if (paciente.idPaciente == idPacientePasado) temp = paciente;
+    });
+
+    return temp;
+  }
+
+  List<Paciente> getCola() {
+    List<Paciente> cola =  List.from(pacientes);
+
+    consultas.forEach((consulta) {
+      if (consulta.idPaciente != null) {
+        pacientes.forEach((paciente) {
+          if (consulta.idPaciente == paciente.idPaciente) cola.remove(paciente);
+        });
+      }
+    });
+
+    return cola;
   }
 }
